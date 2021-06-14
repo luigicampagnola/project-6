aa639df0e0d7fd951456b1daff89001643ec1a51
luigicampagnola <58444283+luigicampagnola@users.noreply.github.com>
2021-06-14 08:45:27 +0200

    up

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -60,7 +60,6 @@ class App extends Component {
       <div>
         <Particles params={particlesConfig} className="particles" />
         <Logo />
-
         <Instructions />
         <ImageLinkForm
           onClickHandler={this.onClickHandler}
